4da6cd606c6bad46b6e06fefffd1b0b9741a1d91
Bencode92 <benoit.comas@gmail.com>
2025-04-17 13:52:47 +0200

    Ajout des courses manquantes dans les données de secours

--- data-loader.js
+++ data-loader.js
@@ -82,6 +82,85 @@ const FALLBACK_DATA = {
                 { n: "3", cheval: "GOLDEN BROWN", jockey: "JEAN-BERNARD EYQUEM", entraineur: "JC. ROUGET (S)", proprietaire: "ECURIE VIVALDI", eleveurs: "HARAS DE GRANDCAMP EARL", poids: "58 kg", performances: "3p 2p(24)4p3p2p3p" },
                 { n: "4", cheval: "BLACK BOSS", jockey: "GUILLAUME MILLET", entraineur: "R. FRADET (S)", proprietaire: "MR FABRICE FANTAUZZA", eleveurs: "SUC. D.DE LA HERONNIERE", poids: "58 kg", performances: "2p2p" }
             ]
+        },
+        {
+            nom: "PRIX DE LA SOCIETE DES COURSES DE SALON-DE-PROVENCE",
+            horaire: "13h27",
+            numero: "4",
+            type: "Plat",
+            participants: [
+                { n: "1", cheval: "PRADO", jockey: "ANTHONY CRASTUS", entraineur: "P. COTTIER", proprietaire: "MR JEAN-PIERRE-JOSEPH DUBOIS", eleveurs: "JPJ. DUBOIS", poids: "57 kg", performances: "4p3p3p1p3p(24)3p2p7p" },
+                { n: "2", cheval: "MONDO", jockey: "JEAN-BERNARD EYQUEM", entraineur: "JC. ROUGET (S)", proprietaire: "ECURIE C. MARZOCCO", eleveurs: "HARAS D'ETREHAM, RIVIERA EQUINE S.A.R.L", poids: "57 kg", performances: "4p(24)7p1p3p" },
+                { n: "3", cheval: "ZARAKCHIC", jockey: "ANTONIO ORANI", entraineur: "J. REYNIER (S)", proprietaire: "G.PARIENTE/JC.SEROUL/AUGUSTIN-N.", eleveurs: "GUY PARIENTE HOLDING", poids: "57 kg", performances: "4p(24)1p3p" },
+                { n: "4", cheval: "MARCHEMALO", jockey: "HUGO BESNIER", entraineur: "P. COTTIER", proprietaire: "JF.GRIBOMONT/EC.SUD/D.DUMOULIN", eleveurs: "JF. GRIBOMONT", poids: "57 kg", performances: "1p" },
+                { n: "5", cheval: "DIOPTASE (IRE)", jockey: "MARVIN GRANDIN", entraineur: "JC. ROUGET (S)", proprietaire: "LE MARAIS/EC.STEMPNIAK/PARIENTE", eleveurs: "SCEA MARMION VAUVILLE, ECURIE SKYMARC FARM, NIGHTCHILL LLC", poids: "57 kg", performances: "2p5p1p" },
+                { n: "6", cheval: "STEF LE MAGNIFIQUE", jockey: "GUILLAUME MILLET", entraineur: "R. FRADET (S)", proprietaire: "MR MICHEL NIKITAS", eleveurs: "W. NEUMANN, EARL HARAS DU LOGIS, J. INCE", poids: "57 kg", performances: "6p2p(24)2p1p" },
+                { n: "7", cheval: "KLYCOT", jockey: "SYLVAIN RUIS", entraineur: "N. PERRET (S)", proprietaire: "MR JEAN-CLAUDE SEROUL", eleveurs: "JC. SEROUL", poids: "57 kg", performances: "1p6p" }
+            ]
+        },
+        {
+            nom: "PRIX DU CENTRE D'ENTRAINEMENT DE CALAS",
+            horaire: "14h12",
+            numero: "5",
+            type: "Plat",
+            participants: [
+                { n: "1", cheval: "BOOGIE", jockey: "ANTHONY CRASTUS", entraineur: "N. PERRET (S)", proprietaire: "MR HERVE GUILLET", eleveurs: "RY. SIMON, N. SIMON, MME V. SIMON, MME C. SIMON", poids: "59 kg", performances: "1p1p2p 0p1p(24)2p1p9p2p" },
+                { n: "2", cheval: "INTERSTELLA GER", jockey: "MME MARINA BRUNELLI", entraineur: "MME M. SCANDELLA-LACAILLE", proprietaire: "ECURIE JOYA RACING SAS", eleveurs: "MME U. IMM, J. IMM", poids: "54 kg(57,5 kg)", performances: "1p4p 6p7p9p(24)4p1p6p5p" },
+                { n: "3", cheval: "ZLARA", jockey: "IORITZ MENDIZABAL", entraineur: "M. CESANDRI (S)", proprietaire: "MR JEAN-CLAUDE RAVIER", eleveurs: "BLOODSTOCK AGENCY LTD, MME M. RISI", poids: "57,5 kg", performances: "(24)1p8p8p4p 4p0p1p1p2p" },
+                { n: "4", cheval: "POLYSPEED (IRE)", jockey: "ANTONIO ORANI", entraineur: "MME M. SCANDELLA-LACAILLE", proprietaire: "AA.AMELINE/AGV KARWIN/EC.EQUUS", eleveurs: "WERTHEIMER & FRERE", poids: "57 kg", performances: "1p2p2p(24)5p" },
+                { n: "5", cheval: "BACK TO BLACK", jockey: "MARVIN GRANDIN", entraineur: "J. REYNIER (S)", proprietaire: "ECURIE MG", eleveurs: "F. VERMEULEN", poids: "56 kg", performances: "7p(24)3p4p5p(23)5p6p5p4p" },
+                { n: "6", cheval: "MY CHARMING PRINCE", jockey: "GUILLAUME MILLET", entraineur: "E. MIKHALIDES (S)", proprietaire: "MR STEPHANE GRANDIN", eleveurs: "EARL HARAS DU CAMP BENARD", poids: "56 kg", performances: "0p7p(23)3p1p9p9p 2p6p3p" },
+                { n: "7", cheval: "BLACK SAXON", jockey: "HUGO BESNIER", entraineur: "N. BELLANGER (S)", proprietaire: "MR DIDIER MARS", eleveurs: "LE THENNEY", poids: "56 kg", performances: "6p(24)7p1p3p4p" },
+                { n: "8", cheval: "LADY MANA", jockey: "MME MANON GERMAIN", entraineur: "F. FORESI", proprietaire: "MR GUILLAUME DUROT", eleveurs: "HARAS DU HOGUENET, P. NATAF", poids: "53 kg(54,5 kg)", performances: "9p(24)0p2p4p3p 7p1p0p3p" },
+                { n: "9", cheval: "AMELIELYMPIQUE (IRE)", jockey: "SYLVAIN RUIS", entraineur: "N. PERRET (S)", proprietaire: "MR JEAN-CLAUDE SEROUL", eleveurs: "JC. SEROUL", poids: "54,5 kg", performances: "0p(24)Tp3p5p8p 4p(23)3p5p" }
+            ]
+        },
+        {
+            nom: "PRIX JEAN-JACQUES NAPOLI",
+            horaire: "14h47",
+            numero: "6",
+            type: "Plat",
+            participants: [
+                { n: "1", cheval: "SHAMSABAD", jockey: "ANTONIO ORANI", entraineur: "C. MARTINON", proprietaire: "ECURIE DU PERROQUET SAUVAGE", eleveurs: "SUC. S.A. AGA KHAN", poids: "60 kg", performances: "2p9p6p 6p6p3p8p(24)0p0p" },
+                { n: "2", cheval: "ALROMY", jockey: "MME MANON GERMAIN", entraineur: "MME L. SALTON", proprietaire: "MME LAURA SALTON", eleveurs: "SCA ELEVAGE DE TOURGEVILLE, MME H. ERCULIANI", poids: "58,5 kg(60 kg)", performances: "1p9p0p(24)0p3p9p1p 7p1p" },
+                { n: "3", cheval: "TIGRE ROUGE", jockey: "MICKAEL FOREST", entraineur: "RC. MONTENEGRO", proprietaire: "MME SUSANNE RITSON", eleveurs: "MME S. RITSON", poids: "58,5 kg", performances: "4p3p2p(24)8p2p1p1p 4p1p" },
+                { n: "4", cheval: "DREAM OF EMERAUDE", jockey: "GUILLAUME MILLET", entraineur: "J. VAN HANDENHOVE", proprietaire: "ECURIE DE LA VERTE VALLEE", eleveurs: "MME A. DELARUE", poids: "58,5 kg", performances: "5p0p0p5p(24)1p9p6p3p 6p" },
+                { n: "5", cheval: "MOUTRAKI IRE", jockey: "HUGO BESNIER", entraineur: "N. PERRET (S)", proprietaire: "MR ERIC D' ANGELO", eleveurs: "LIMESTONE STUD, TARA STUD", poids: "58 kg", performances: "6p9p4p(24)8p2p6p7p 0p8p" },
+                { n: "6", cheval: "BUSINESS PLAN", jockey: "MARVIN GRANDIN", entraineur: "S. LABATE (S)", proprietaire: "P.FAUCAMPRE/B.ROTGER/N.MICHELOTT", eleveurs: "V. BUKHTOYAROV, E. KAPPUSHEV", poids: "57 kg", performances: "9p8p9p6p6p(24)4p4p4p6p" },
+                { n: "7", cheval: "FOR BETTY", jockey: "GREGOIRE LEGRAS", entraineur: "N. PERRET (S)", proprietaire: "MME CRISTEL MARTINA", eleveurs: "THOUSAND DREAMS", poids: "57 kg", performances: "2p(24)0p6p" },
+                { n: "8", cheval: "HADLEIGH", jockey: "IORITZ MENDIZABAL", entraineur: "J. ANDREU (S)", proprietaire: "MR ROBERT MINICHIELLO", eleveurs: "ECURIE KURA", poids: "56 kg", performances: "4p0p4p7p 5p(24)0p0p4p4p" }
+            ]
+        },
+        {
+            nom: "PRIX DE MIRAMAS",
+            horaire: "15h22",
+            numero: "7",
+            type: "Plat",
+            participants: [
+                { n: "1", cheval: "COTE JARDIN", jockey: "JIRI CHALOUPKA", entraineur: "J. CHALOUPKA", proprietaire: "CAJDASROT S.R.O.", eleveurs: "F. BOZO, SUC. P. BOZO, MME MC. BOZO, JC. BOZO", poids: "60 kg", performances: "(24)6p0p0p5p(23)0p6p8p6p" },
+                { n: "2", cheval: "AL HABISSET", jockey: "HUGO BESNIER", entraineur: "F. BOUALEM", proprietaire: "MR FAOUZI BOUALEM", eleveurs: "HARAS DU GRAND COURGEON", poids: "59,5 kg", performances: "5p6p 6p3p8p(24)2p3p3p3p" },
+                { n: "3", cheval: "ASPEN", jockey: "ANTONIO ORANI", entraineur: "R. FRADET (S)", proprietaire: "MR MICHEL NIKITAS", eleveurs: "ALEYRION BLOODSTOCK LTD", poids: "59,5 kg", performances: "3p0p0p 0p7p(24)8p1p0p4p" },
+                { n: "4", cheval: "GAMECHANGER", jockey: "EMILIEN PUILLET-RODA", entraineur: "JC. SARAIS", proprietaire: "MR DANIEL ORTU", eleveurs: "ECURIE SKYMARC FARM", poids: "58 kg(59,5 kg)", performances: "1p0p1p9p 5p4p(24)6p7p6p" },
+                { n: "5", cheval: "INTO FAITH", jockey: "MME CORALIE PACAUT", entraineur: "C. ESCUDER", proprietaire: "MR ALEXANDRE DE LANFRANCHI", eleveurs: "THE KATHRYN STUD", poids: "58 kg(59,5 kg)", performances: "4p1p7p6p6p(24)5p2p2p1p" },
+                { n: "6", cheval: "LESSARD", jockey: "MME MICKAELLE MICHEL", entraineur: "MME M. SCANDELLA-LACAILLE", proprietaire: "MR GERARD AUGUSTIN-NORMAND", eleveurs: "FRANKLIN FINANCE S.A.", poids: "58 kg(59,5 kg)", performances: "2p0p9p9p2p(24)5p1p1p2p" },
+                { n: "7", cheval: "EXHIT", jockey: "RUDY PIMBONNET", entraineur: "M. PIMBONNET", proprietaire: "B.BIMOZ/ECURIE SAINT-HILAIRE", eleveurs: "WERTHEIMER & FRERE", poids: "58 kg", performances: "8p0p1p1p 2p7p(24)0p2p6p" }
+            ]
+        },
+        {
+            nom: "PRIX CHRISTIAN FORNAROLI",
+            horaire: "15h57",
+            numero: "8",
+            type: "Plat",
+            participants: [
+                { n: "1", cheval: "LOU MAN", jockey: "GREG BENOIST", entraineur: "P. DAULIER", proprietaire: "MME SYLVIE DAULIER", eleveurs: "K. RHATIGAN", poids: "60 kg", performances: "4p 0p0p(24)7p6p0p0p0p0p" },
+                { n: "2", cheval: "ROSEDALE", jockey: "SYLVAIN RUIS", entraineur: "P. COTTIER", proprietaire: "MR JEAN-PIERRE-JOSEPH DUBOIS", eleveurs: "JPJ. DUBOIS", poids: "60 kg", performances: "2p(24)2p4p 2p1p3p 5p7p" },
+                { n: "3", cheval: "YATT", jockey: "MARVIN GRANDIN", entraineur: "J. REYNIER (S)", proprietaire: "ECURIE NICOLAS", eleveurs: "HARAS DE LA PERELLE", poids: "59 kg", performances: "(24)3p1p4p5p(23)5p1p2p4p" },
+                { n: "4", cheval: "ACCLAM", jockey: "ANTHONY CRASTUS", entraineur: "P. COTTIER", proprietaire: "EC.DES CHARMES/MR R.TALBOT", eleveurs: "THE ACCLAMATION SYNDICATE", poids: "58,5 kg", performances: "0p 1p(24)0p8p6p(23)6p(22)" },
+                { n: "5", cheval: "BELLAGIO", jockey: "HUGO BESNIER", entraineur: "N. PERRET (S)", proprietaire: "MR JEAN-CLAUDE SEROUL", eleveurs: "JC. SEROUL", poids: "58,5 kg", performances: "9p4p(24)1p8p1p2p 7p2p6p" },
+                { n: "6", cheval: "BONS BAISERS", jockey: "ANTONIO ORANI", entraineur: "P. COTTIER", proprietaire: "EC.DES CHARMES/MME V.FAVE", eleveurs: "LA TOUQUES BLOODSTOCK", poids: "57,5 kg", performances: "6p(24)9p0p3p0p 2p(23)3p" },
+                { n: "7", cheval: "LYKION", jockey: "MICKAEL FOREST", entraineur: "RC. MONTENEGRO", proprietaire: "E.BUCHER/J.SCHOENENBERGER", eleveurs: "G. KERN, E. BUCHER", poids: "57,5 kg", performances: "1p(24)9p5p0p 5p4p8p0p0p" },
+                { n: "8", cheval: "STAR VICTORY", jockey: "IORITZ MENDIZABAL", entraineur: "M. CESANDRI (S)", proprietaire: "MR JEAN-CLAUDE SEROUL", eleveurs: "JC. SEROUL", poids: "57,5 kg", performances: "9p5p(24)0p0p1p5p3p 0p3p" }
+            ]
         }
     ],
     "LONGCHAMP": [
@@ -94,6 +173,47 @@ const FALLBACK_DATA = {
                 { n: "1", cheval: "GALACTIC STAR", jockey: "CHRISTOPHE SOUMILLON", entraineur: "A. FABRE (S)", proprietaire: "GODOLPHIN SNC", eleveurs: "DARLEY", poids: "58 kg", performances: "1p1p2p" },
                 { n: "2", cheval: "SWIFT VICTORY", jockey: "MAXIME GUYON", entraineur: "F. HEAD (S)", proprietaire: "WERTHEIMER & FRERE", eleveurs: "WERTHEIMER ET FRERE", poids: "58 kg", performances: "2p1p3p" },
                 { n: "3", cheval: "URBAN LEGEND", jockey: "MICKAEL BARZALONA", entraineur: "A. FABRE (S)", proprietaire: "GODOLPHIN SNC", eleveurs: "DARLEY", poids: "57 kg", performances: "2p3p1p" }
+            ]
+        }
+    ],
+    "PARISLONGCHAMP": [
+        {
+            nom: "PRIX DE LA SEINE",
+            horaire: "15h15",
+            numero: "1",
+            type: "Plat",
+            participants: [
+                { n: "1", cheval: "ROYAL DESTINY", jockey: "STEPHANE PASQUIER", entraineur: "C. LAFFON-PARIAS", proprietaire: "WERTHEIMER & FRERE", eleveurs: "WERTHEIMER ET FRERE", poids: "58 kg", performances: "2p1p" },
+                { n: "2", cheval: "DIAMOND LIGHT", jockey: "PIERRE-CHARLES BOUDOT", entraineur: "A. FABRE (S)", proprietaire: "GODOLPHIN SNC", eleveurs: "DARLEY", poids: "58 kg", performances: "1p2p" }
+            ]
+        }
+    ],
+    "SAN SEBASTIAN": [
+        {
+            nom: "GRAN PREMIO DE SAN SEBASTIAN",
+            horaire: "16h30",
+            numero: "1",
+            type: "Plat",
+            participants: [
+                { n: "1", cheval: "SUENA EL DEMBOW", jockey: "VACLAV JANACEK", entraineur: "H. PEREIRA", proprietaire: "YEGUADA ROCIO", eleveurs: "YEGUADA ROCIO", poids: "57 kg", performances: "1p1p2p" },
+                { n: "2", cheval: "JOUMA", jockey: "RICARDO SOUSA", entraineur: "G. ARIZKORRETA", proprietaire: "IVAES", eleveurs: "IVAES", poids: "57 kg", performances: "2p1p3p" },
+                { n: "3", cheval: "ALCAZAR DE SEGOVIA", jockey: "BORJA FAYOS", entraineur: "O. ANAYA", proprietaire: "NANINA", eleveurs: "NANINA", poids: "57 kg", performances: "3p2p1p" },
+                { n: "4", cheval: "GANO Y VUELVO", jockey: "JAIME GELABERT", entraineur: "E. LEON", proprietaire: "TINERFE", eleveurs: "TINERFE", poids: "57 kg", performances: "4p3p1p" },
+                { n: "5", cheval: "WHITE SPIRIT", jockey: "JOSE LUIS BORREGO", entraineur: "J. CALDERÓN", proprietaire: "ODISEA", eleveurs: "ODISEA", poids: "57 kg", performances: "5p4p2p" }
+            ]
+        }
+    ],
+    "ARG PALERMO": [
+        {
+            nom: "GRAN PREMIO PALERMO",
+            horaire: "21h45",
+            numero: "1",
+            type: "Plat",
+            participants: [
+                { n: "1", cheval: "EL MAGNIFICO", jockey: "EDUARDO ORTEGA", entraineur: "R. PELLEGATTA", proprietaire: "HARAS LA QUEBRADA", eleveurs: "HARAS LA QUEBRADA", poids: "57 kg", performances: "1p1p2p" },
+                { n: "2", cheval: "ROMAN EMPEROR", jockey: "PABLO FALERO", entraineur: "J.C. ETCHECHOURY", proprietaire: "HARAS FIRMAMENTO", eleveurs: "HARAS FIRMAMENTO", poids: "57 kg", performances: "2p1p3p" },
+                { n: "3", cheval: "WINNING CAUSE", jockey: "WILSON MOREYRA", entraineur: "N. MARTIN FERRO", proprietaire: "HARAS VACACION", eleveurs: "HARAS VACACION", poids: "57 kg", performances: "1p3p2p" },
+                { n: "4", cheval: "MASTER OF HOUNDS", jockey: "FRANCISCO GONCALVES", entraineur: "A. GAITAN", proprietaire: "STUD MATRERA", eleveurs: "STUD MATRERA", poids: "57 kg", performances: "3p2p1p" }
             ]
         }
     ]
